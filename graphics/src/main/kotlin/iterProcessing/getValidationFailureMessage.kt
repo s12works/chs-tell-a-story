03649c1d8b3ebd8fef6cd002dadb2810551accf2
Saunder Stock <saunderstock2@gmail.com>
2024-12-15 23:39:39 -0500

    create getValidationFailureMessage.kt

---
+++ graphics/src/main/kotlin/iterProcessing/getValidationFailureMessage.kt
@@ -0,0 +1,4 @@
+package com.s12works.tellAStory.graphics.iterProcessing
+
+public fun getValidationFailureMessage(int: TimeoutInteger): com.s12works.tellAStory.assertion.Message =
+    "TimeoutInt provided (${int}) is illegal"
