12aadd7c3d8d8e2ebfc64553549966e2aef6cc1c
Saunder Stock <saunderstock2@gmail.com>
2024-12-15 23:38:52 -0500

    create ProcessHandlerConfig.kt

---
+++ graphics/src/main/kotlin/iterProcessing/ProcessHandlerConfig.kt
@@ -0,0 +1,32 @@
+package com.s12works.tellAStory.graphics.iterProcessing
+
+import com.s12works.tellAStory.range.contains
+import com.s12works.tellAStory.assertion.enforcers.fail
+
+public class ProcessHandlerConfig<I> {
+    public val iterator: Iterator<I>
+    public val timeout: TimeoutInteger
+    public val process: Process<I>
+
+    public constructor(iterator: Iterator<I>, timeout: TimeoutInteger, process: Process<I>) {
+        this.iterator = iterator
+        this.timeout = com.s12works.tellAStory.assertion.validated(
+            getValidationFailureMessage(timeout),
+            ::fail,
+            timeout
+        ) {
+            timeout in com.s12works.tellAStory.ranges.positive
+        }
+        this.process = process
+    }
+    
+    override fun toString(): com.s12works.tellAStory.objectRepresentation.Translation =
+        com.s12works.tellAStory.objectRepresentation.getStandardRepr(this)
+
+    override fun equals(other: Any?): Boolean = (
+        other is ProcessHandlerConfig<*> &&
+        this.iterator == other.iterator &&
+        this.timeout == other.timeout &&
+        this.process == other.process
+    )
+}
