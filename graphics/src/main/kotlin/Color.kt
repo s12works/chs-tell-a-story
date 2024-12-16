724aa20a19873adbb9bb7b9d23ac951ca09c7e89
Saunder Stock <saunderstock2@gmail.com>
2024-12-16 15:22:09 -0500

    update Color.kt

--- graphics/src/main/kotlin/Color.kt
+++ graphics/src/main/kotlin/Color.kt
@@ -1,12 +1,26 @@
 package com.s12works.tellAStory.graphics
 
-import com.s12works.tellAStory.graphics.rgb.IndexValue
+import com.s12works.tellAStory.graphics.colorUtil.IndexValue
+import com.s12works.tellAStory.graphics.colorUtil.validated
 
-public data class Color(
-    public val red: IndexValue,
-    public val green: IndexValue,
+public class Color {
+    public val red: IndexValue
+    public val green: IndexValue
     public val blue: IndexValue
-) {
+
+    public constructor(red: IndexValue, green: IndexValue, blue: IndexValue) {
+        this.red = validated(red)
+        this.green = validated(green)
+        this.blue = validated(blue)
+    }
+
     override fun toString(): com.s12works.tellAStory.objectRepresentation.Translation =
         com.s12works.tellAStory.objectRepresentation.getStandardRepr(this)
+
+    override fun equals(other: Any?): Boolean = (
+        other is Color &&
+        this.red == other.red &&
+        this.green == other.green &&
+        this.blue == other.blue
+    )
 }
