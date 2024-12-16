a07e9cd3188a8c4cd325b90d47e53f162dbbb305
Saunder Stock <saunderstock2@gmail.com>
2024-12-15 23:43:26 -0500

    create getValidationfailureMessage.kt

---
+++ graphics/src/main/kotlin/rgb/getValidationFailureMessage.kt
@@ -0,0 +1,4 @@
+package com.s12works.tellAStory.graphics.rgb
+
+public fun getValidationFailureMessage(color: com.s12works.tellAStory.graphics.Color): com.s12works.tellAStory.assertion.Message =
+    "color validation assertion failed; $color is illegal"
