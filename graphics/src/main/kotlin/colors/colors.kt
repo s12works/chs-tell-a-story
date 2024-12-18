ba7026a3aad7e1523c6e2c482ec13bacabcb9dd4
Saunder Stock <saunderstock2@gmail.com>
2024-12-17 22:02:13 -0500

    create colors/colors.kt

---
+++ graphics/src/main/kotlin/colors/colors.kt
@@ -0,0 +1,53 @@
+package com.s12works.tellAStory.graphics.colors
+
+import com.s12works.tellAStory.graphics.colorUtil.range
+import com.s12works.tellAStory.graphics.Color
+
+@FromStandardLibrary
+public val red = Color(
+    range.maximum!!,
+    range.minimum!!,
+    range.minimum!!
+) 
+
+@FromStandardLibrary
+public val green = Color(
+    range.minimum!!,
+    range.maximum!!,
+    range.minimum!!
+)
+
+@FromStandardLibrary
+public val blue = Color(
+    range.minimum!!,
+    range.minimum!!,
+    range.maximum!!
+)
+
+@FromStandardLibrary
+public val black = Color(
+    range.minimum!!,
+    range.minimum!!,
+    range.minimum!!
+)
+
+@FromStandardLibrary
+public val white = Color(
+    range.maximum!!,
+    range.maximum!!,
+    range.maximum!!
+)
+
+@FromStandardLibrary
+public val yellow = Color(
+    range.maximum!!,
+    range.maximum!!,
+    range.minimum!!
+)
+
+@FromStandardLibrary
+public val purple = Color(
+    range.maximum!!,
+    range.minimum!!,
+    range.maximum!!
+)
