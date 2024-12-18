a10ae1217cec6037a0a24630d15f6400f5fec1a5
Saunder Stock <saunderstock2@gmail.com>
2024-12-17 22:03:18 -0500

    create positionUtil/InfixOperator.kt

---
+++ graphics/src/main/kotlin/positionUtil/InfixOperator.kt
@@ -0,0 +1,5 @@
+package com.s12works.tellAStory.graphics.positionUtil
+
+import com.s12works.tellAStory.graphics.Coordinate
+
+public typealias InfixOperator = (Coordinate, Coordinate) -> Coordinate
