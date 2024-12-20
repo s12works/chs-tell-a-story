5718d59151a438c9b1c2d31d1366a5e637d7d572
s12works <saunderstock2@gmail.com>
2024-12-20 11:44:31 -0700

    update Reciept.kt

--- assertion/src/main/kotlin/assertion/Reciept.kt
+++ assertion/src/main/kotlin/assertion/Reciept.kt
@@ -1,9 +1,21 @@
 package com.s12works.tellAStory.assertion
 
+/**
+ * consists of all vital components required for representing/identifying an assertion failure
+ *
+ * @property message serves as the respective error-message
+ * @property assessor represents the function that provided the assessment
+ */
 public data class Reciept(
     public val message: Message,
     public val assessor: () -> Assessment
 ) {
+    /**
+     * provides a string-representation of the object
+     *
+     * @return string-representation provided as [com.s12works.tellAStory.objectRepresentation.Translation]
+     * @see com.s12works.tellAStory.objectRepresentation.getStandardRepr
+     */
     override fun toString(): com.s12works.tellAStory.objectRepresentation.Translation =
         com.s12works.tellAStory.objectRepresentation.getStandardRepr(this)
 }
