941a73d2ddb15b9fd636c868fb58202044b81fa5
s12works <saunderstock2@gmail.com>
2024-12-20 11:53:23 -0700

    update assert.kt

--- assertion/src/main/kotlin/assertion/assert.kt
+++ assertion/src/main/kotlin/assertion/assert.kt
@@ -1,5 +1,14 @@
 package com.s12works.tellAStory.assertion
 
+/**
+ * an action is performed via [enforcer] based on the potentially [com.s12works.tellAStory.assertion.Report] progided by [assessor]
+ *
+ * @param message represents the desired error-message (as [com.s12works.tellAStory.assertion.Message])
+ * @param enforcer represents/serves as the desired [com.s12works.tellAStory.assertion.Enforcer] for handling the potential [com.s12works.tellAStory.assertion.Report] provided by [assessor]
+ * @param assessor serves as the desired assessor used for determining whether or not a report is instantiated
+ *
+ * @return returns [Unit]; function intended to perform action via enforcer if necessary only
+ */
 public fun assert(
     message: Message,
     enforcer: Enforcer<*>,
