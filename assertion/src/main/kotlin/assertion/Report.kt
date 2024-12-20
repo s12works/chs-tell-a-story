63d24b5d38cfcb32a4424162a0018775ccee51b8
s12works <saunderstock2@gmail.com>
2024-12-20 11:45:05 -0700

    update Report.kt

--- assertion/src/main/kotlin/assertion/Report.kt
+++ assertion/src/main/kotlin/assertion/Report.kt
@@ -10,6 +10,12 @@ public data class Report(
     public val reciept: Reciept,
     public val occurance: java.time.LocalDateTime
 ) {
+    /**
+     * provides a string-representation of the object
+     *
+     * @return string-representation provided as [com.s12works.tellAStory.objectRepresentation.Translation]
+     * @see com.s12works.tellAStory.objectRepresentation.getStandardRepr
+     */
     override fun toString(): com.s12works.tellAStory.objectRepresentation.Translation =
         com.s12works.tellAStory.objectRepresentation.getStandardRepr(this)
 } 
