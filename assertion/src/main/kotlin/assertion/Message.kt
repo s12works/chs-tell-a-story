f8a86b92fe0ba78708e8ef53927f2d0177aee3e8
s12works <saunderstock2@gmail.com>
2024-12-20 10:07:43 -0700

    update Message.kt

--- assertion/src/main/kotlin/assertion/Message.kt
+++ assertion/src/main/kotlin/assertion/Message.kt
@@ -1,3 +1,8 @@
 package com.s12works.tellAStory.assertion
 
+/**
+ * represents message defined as part of [com.s12works.tellAStory.assertion.Reciept]
+ *
+ * typeslias is defined in terms of [com.s12works.tellAStory.ExceptionMessage]
+ */
 public typealias Message = com.s12works.tellAStory.ExceptionMessage
