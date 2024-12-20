c43d2262769e0cee93df08fe4a8e05fe68e8cd19
s12works <saunderstock2@gmail.com>
2024-12-20 10:04:00 -0700

    update Enforcer.kt

--- assertion/src/main/kotlin/assertion/Enforcer.kt
+++ assertion/src/main/kotlin/assertion/Enforcer.kt
@@ -1,3 +1,8 @@
 package com.s12works.tellAStory.assertion
 
+/**
+ * defined as the "handler" that conducts the desired action(s) for a [com.s12works.tellAStory.assertion.Report]
+ *
+ * @param O represents type of object provided by [Enforcer]
+ */
 public typealias Enforcer<O> = (Report) -> O
